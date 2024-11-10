6fb9dcdbfc6b83ee13a1815ceb640f6f9bc4e3ed
prostodasha62@gmail.com <prostodasha62@gmail.com>
2024-11-10 22:02:14 +0500

    Task4

---
+++ Tyuiu.KorobeinikovaDD.Sprint5.Task4.V28.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.KorobeinikovaDD.Sprint5.Task4.V28.Lib
+{
+    public class DataService : ISprint5Task4V28
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string str = File.ReadAllText(path);
+            string str1 = str.Replace(".", ",");
+            double x = Convert.ToDouble(str1);
+            double res = Math.Round(3 * Math.Pow(x, 3) / Math.Sin(x), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KorobeinikovaDD.Sprint5.Task4.V28/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.KorobeinikovaDD.Sprint5.Task4.V28.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #5 | Выполнила: Коробейникова Д.Д. | ПКТб-24-1";
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* Тема: Чтение данных из текстового файла                                     *");
+Console.WriteLine("* Задание #4                                                                  *");
+Console.WriteLine("* Вариант #28                                                                 *");
+Console.WriteLine("* Выполнила: Коробейникова Дарья Денисовна | ПКТб-24-1                        *");
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                    *");
+Console.WriteLine("* Прочитать значение х из файла и вычислить значение по формуле               *");
+Console.WriteLine("*                                                                             *");
+Console.WriteLine("*                                                                             *");
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                            *");
+Console.WriteLine("*******************************************************************************");
+
+string path = @"C:\DataSprint5\InPutDataFileTask4V28.txt";
+
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                                  *");
+Console.WriteLine("*******************************************************************************");
+double res = ds.LoadFromDataFile(path);
+Console.WriteLine(res);
+
+Console.ReadKey();
