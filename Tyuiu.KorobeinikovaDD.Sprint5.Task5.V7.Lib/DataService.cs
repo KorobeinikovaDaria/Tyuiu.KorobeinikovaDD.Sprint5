f19882b10c665c87455c6c78fc2e2678f5aee277
prostodasha62@gmail.com <prostodasha62@gmail.com>
2024-11-16 01:05:54 +0500

    Task5

--- Tyuiu.KorobeinikovaDD.Sprint5.Task5.V7.Lib/DataService.cs
+++ Tyuiu.KorobeinikovaDD.Sprint5.Task5.V7.Lib/DataService.cs
@@ -1,27 +1,27 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.KorobeinikovaDD.Sprint5.Task5.V7.Lib
 {
     public class DataService : ISprint5Task5V7
     {
         public double LoadFromDataFile(string path)
         {
-            string[] str = path.Split(',');
             double res = 1;
-            using (StreamReader reader = new StreamReader(path))
+            using StreamReader sr = new StreamReader(path);
             {
-                var line = reader.ReadLine();
-                while (line != null)
+                var line = sr.ReadLine();
+                while ( line != null)
+                {
+                    double n = Convert.ToDouble(line);
+                    for ( int i = 1; i <= n; i++)
                     {
-                     double n = Convert.ToDouble(line);
-                     for (int i = 1; i <= n; i++)
-                        {
-                            res *= i;
-                        }
-                        break;
+                        res *= i;
                     }
-             
-                return res;
+                    break;
+                }
             }
+            return res;
         }
     }
-}
+    }
+
