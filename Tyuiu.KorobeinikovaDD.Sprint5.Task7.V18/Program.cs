6c913b96aefbcd1672954ba5f4d1193c473250b5
prostodasha62@gmail.com <prostodasha62@gmail.com>
2024-11-17 21:38:07 +0500

    Task7

---
+++ Tyuiu.KorobeinikovaDD.Sprint5.Task7.V18/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.KorobeinikovaDD.Sprint5.Task7.V18.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #5 | Выполнила: Коробейникова Д.Д. | ПКТб-24-1";
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                     *");
+Console.WriteLine("* Задание #7                                                                  *");
+Console.WriteLine("* Вариант #18                                                                 *");
+Console.WriteLine("* Выполнила: Коробейникова Дарья Денисовна | ПКТб-24-1                        *");
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                    *");
+Console.WriteLine("* Заменить все буквы \"н\" на \"нн\".Полученный результат сохранить           *");
+Console.WriteLine("* в файл OutPutDataFileTask7V18.txt.                                          *");
+Console.WriteLine("*                                                                             *");
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                            *");
+Console.WriteLine("*******************************************************************************");
+
+string path = @"C:\DataSprint5\InPutDataFileTask7V18.txt";
+Console.WriteLine("Данные находятся в файле: " + path);
+
+Console.WriteLine("*******************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                                  *");
+Console.WriteLine("*******************************************************************************");
+string res = ds.LoadDataAndSave(path);
+Console.WriteLine("Результат находится в файле: " + res);
+
+
+
+Console.ReadKey();
